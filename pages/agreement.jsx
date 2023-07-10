e9c945b7e36fa69be92a18dc77654c7423bfd181
Ozan Deniz Demirtas <ozanddemirtas@gmail.com>
2023-07-10 13:42:27 +0300

    sso temp new screens

---
+++ pages/agreement.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import "./style.css";
+
+export const MacbookPro = () => {
+  return (
+    <div className="macbook-pro">
+      <div className="div">
+        <div className="overlap-group">
+          <div className="overlap">
+            <div className="text-wrapper">DEVAM</div>
+          </div>
+          <div className="overlap-2">
+            <div className="text-wrapper-2">Sözleşme Başlık</div>
+            <p className="lorem-ipsum-is">
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+              industry&#39;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
+              scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap
+              typesetting industry. <br />
+              <br />
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+              industry&#39;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
+              scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap
+              typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
+              Ipsum has been the industry&#39;s standard dummy text ever since the 1500s <br />
+              <br />
+              Type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the
+              leap typesetting industry.
+            </p>
+          </div>
+          <div className="text-wrapper-3">Okudum, kabul ediyorum.</div>
+          <div className="ellipse-wrapper">
+            <div className="ellipse" />
+          </div>
+        </div>
+        <footer className="footer">
+          <h1 className="h-1">0 850 222 8222</h1>
+          <div className="text-wrapper-4">7/24 İletişim Merkezi</div>
+          <div className="text-wrapper-5">Müşteri Ol</div>
+          <div className="text-wrapper-6">Bize Ulaşın</div>
+          <img className="line" alt="Line" src="line-1.svg" />
+        </footer>
+        <header className="header">
+          <div className="text-wrapper-7">English</div>
+          <img className="group" alt="Group" src="group.png" />
+        </header>
+        <div className="div-wrapper">
+          <div className="ellipse-2" />
+        </div>
+      </div>
+    </div>
+  );
+};
---
+++ pages/security_img.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import "../security_img.scss";
+
+export const MacbookPro = () => {
+  return (
+    <div className="macbook-pro">
+      <div className="div">
+        <div className="overlap-group">
+          <div className="overlap">
+            <div className="text-wrapper">DEVAM</div>
+          </div>
+          <div className="group">
+            <img className="guvenlik" alt="Guvenlik" src="guvenlik-01-1.png" />
+            <img className="img" alt="Guvenlik" src="guvenlik-02-1.png" />
+            <img className="guvenlik-2" alt="Guvenlik" src="guvenlik-03-1.png" />
+            <img className="guvenlik-3" alt="Guvenlik" src="guvenlik-04-1.png" />
+            <img className="guvenlik-4" alt="Guvenlik" src="guvenlik-05-1.png" />
+            <img className="guvenlik-5" alt="Guvenlik" src="guvenlik-06-1.png" />
+            <img className="guvenlik-6" alt="Guvenlik" src="guvenlik-07-1.png" />
+            <img className="guvenlik-7" alt="Guvenlik" src="guvenlik-08-1.png" />
+            <img className="guvenlik-8" alt="Guvenlik" src="guvenlik-09-1.png" />
+          </div>
+          <div className="text-wrapper-2">Güvenlik Resmi Seçin</div>
+        </div>
+        <footer className="footer">
+          <h1 className="h-1">0 850 222 8222</h1>
+          <div className="text-wrapper-3">7/24 İletişim Merkezi</div>
+          <div className="text-wrapper-4">Müşteri Ol</div>
+          <div className="text-wrapper-5">Bize Ulaşın</div>
+          <img className="line" alt="Line" src="line-1.svg" />
+        </footer>
+        <header className="header">
+          <div className="text-wrapper-6">English</div>
+          <img className="group-2" alt="Group" src="group.png" />
+        </header>
+      </div>
+    </div>
+  );
+};
